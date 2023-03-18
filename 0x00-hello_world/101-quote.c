4e26f6e3f96e3ebb5914fa82365f577b539d0032
MIMO <eng.mg.education@gmail.com>
2023-03-18 06:15:21 -0700

    8. UNIX is basically a simple operating system, but you have to be a genius to understand the simplicity

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,10 +1,16 @@
 #include <stdio.h>
+#include <unistd.h>
 /**
  * main - Entry point
  *
  * Return: Always 1 (Success)
- */
-int write(int filedes, const void *buf, unsigned int nbyte);
+ *
+ * write - String writer function
+ * @filedes: read user's input
+ * @buf: buffer
+ * @nbyte: size of buffer +1
+*/
+int write(int filedes, const char *buf, unsigned int nbyte);
 int main(void)
 {
 	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
