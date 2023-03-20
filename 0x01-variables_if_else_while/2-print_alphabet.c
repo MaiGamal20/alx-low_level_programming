8048f5fe6297f19bb77d5c13b30bfd129f1011ef
MIMO <eng.mg.education@gmail.com>
2023-03-19 18:12:29 -0700

    2. I sometimes suffer from insomnia. And when I can't fall asleep, I play what I call the alphabet game

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/* using putchar */
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	string[] alpha;
+
+	alpha = {a..Z};
+	putchar(alpha);
+	return (0);
+}
