fa46f830acd6fa5d34346f9f8c52bcb74d931eb2
MIMO <eng.mg.education@gmail.com>
2023-03-25 21:03:55 -0700

    0. _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,5 +1,4 @@
-#include <main.h>
-#include <stdio.h>
+#include "main.h"
 /**
  * main - Entry point
  *
@@ -7,10 +6,15 @@
  */
 int main(void)
 {
-	int phrase = "_puchar";
-	
-	putchar(phrase);
-	putchar("\n");
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 	return (0);
 }
 
