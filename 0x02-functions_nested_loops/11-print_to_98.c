fa10294bc11dc16d641599146b6a1c4dd7b26a64
MIMO <eng.mg.education@gmail.com>
2023-03-30 16:54:21 -0700

    11. 98 Battery Street, the OG

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,35 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_to_98 - a function that prints all natural numbers from n
+ * to 98, followed by a new line.
+ * @n: the start num to count from
+ */
+void print_to_98(int n)
+{
+	if (n < 98)
+	{
+		for (n = n ; n <= 98 ; n++)
+		{
+			printf("%d, ", n);
+			if (n == 98)
+			{
+				printf("%d", n);
+			}
+
+		}
+		printf("\n");
+	}
+	else
+	{
+		for (n = n; n >= 98; n--)
+		{
+			printf("%d, ", n);
+			if (n == 98)
+			{
+				printf("%d", n);
+			}
+		}
+		printf("\n");
+	}
+}
