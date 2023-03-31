054d5492df6d797ede054c78860f579e05085b08
MIMO <eng.mg.education@gmail.com>
2023-03-30 17:00:46 -0700

    11. 98 Battery Street, the OG

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -11,7 +11,10 @@ void print_to_98(int n)
 	{
 		for (n = n ; n <= 98 ; n++)
 		{
+			if (n != 98)
+			{
 			printf("%d, ", n);
+			}
 			if (n == 98)
 			{
 				printf("%d", n);
@@ -24,7 +27,10 @@ void print_to_98(int n)
 	{
 		for (n = n; n >= 98; n--)
 		{
+			if (n != 98)
+			{
 			printf("%d, ", n);
+			}
 			if (n == 98)
 			{
 				printf("%d", n);
