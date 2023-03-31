4f4e624fc2a3c3c26e9f90344d4504e504247485
MIMO <eng.mg.education@gmail.com>
2023-03-30 19:34:46 -0700

    2. strncpy

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * _strncpy - a function that copies a string.
+ * @des: destination input
+ * @src: source input
+ * @n: number of bytes inpuet
+ * Return: Return a pointer to the resulting string dest
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+	i = 0;
+	while (i < n && src[i] != '\0')
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	while (i < n)
+	{
+		dest[i] = '\0';
+		i++;
+	}
+	return (dest);
+}
