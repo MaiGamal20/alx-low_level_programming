5acc3130c13ba9798dbc59dcd6783e67d2a5fbb8
MIMO <eng.mg.education@gmail.com>
2023-04-05 04:27:43 -0700

    4. Once an idea has taken hold of the brain it's almost impossible to eradicate

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _pow_recursion - function that returns the value of x raised
+ * to the power of y.
+ * @x: the base number
+ * @y: the power number
+ * Return: x to the power y
+ */
+
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+	{
+		return (-1);
+	}
+	else
+	{
+		if (y == 0)
+		{
+			return (1);
+		}
+		else
+		{
+			return (x * _pow_recursion(x, (y - 1)));
+		}
+	}
+}
